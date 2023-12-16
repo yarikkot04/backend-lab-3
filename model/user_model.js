62806b57e0f0ad7996e24819e3304d138747a19d
yarikkot04 <yarikkotenkoim13@gmail.com>
2023-12-16 22:42:56 +0200

    Added ODM model User.

--- model/user_model.js
+++ model/user_model.js
@@ -1,56 +1,15 @@
-const { v4: uuidv4 } = require('uuid')
-const fs = require('fs')
-const path = require('path')
+const { Schema, model } = require('mongoose')
 
-class User {
-    constructor(name) {
-        this.id = uuidv4()
-        this.name = name
+const userSchema = new Schema({
+    name : {
+        type : String,
+        required : true,
+    },
+    default_currency : {
+        type : String,
+        required : true,
+        default: '$',
     }
+})
 
-    async save() {
-        const users = await User.getAll()
-        users.push({ id: this.id, name: this.name })
-        return new Promise((resolve, reject) => {
-            fs.writeFile(path.resolve(__dirname, '../data/user_list.json'), JSON.stringify(users), (err) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(users[users.length - 1])
-                }
-            })
-        })
-    }
-    static async removeUser(id) {
-        let users = await User.getAll()
-        users = users.filter(u => u.id !== id)
-        return new Promise((resolve, reject) => {
-            fs.writeFile(path.resolve(__dirname, '../data/user_list.json'), JSON.stringify(users), (err) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(users)
-                }
-            })
-        })
-    }
-
-    static async getAll() {
-        return new Promise((resolve, reject) => {
-            fs.readFile(path.resolve(__dirname, '../data/user_list.json'), 'utf-8', (err, data) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(JSON.parse(data))
-                }
-            })
-        })
-    }
-    static async findById(id) {
-        const users = await User.getAll()
-        const selectedUser = users.find(u => u.id === id)
-        return selectedUser
-    }
-}
-
-module.exports = User
+module.exports = model('User', userSchema)
